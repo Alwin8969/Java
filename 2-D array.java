d0ad0ab701358457191a61254726055cd69aab1e
Alwin Joshy <alwinjoshy2028@cs.sjcetpalai.ac.in>
2025-08-19 14:38:16 +0530

    Update 2-D array.java

--- 2-D array.java
+++ 2-D array.java
@@ -2,15 +2,19 @@ import java.util.Scanner;
 public class Matrix {
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
-		int [][] matrix= {
-				{1,2,3},
-				{4,5,6,7},
-				{7,8},
-				
-				};
-		for(int i=0;i<matrix.length;i++) {
-			for(int j=0;j<matrix[i].length;j++) {
-				System.out.println(matrix[i][j]+"\t");
+		System.out.println("Enter the rows and cols");
+		int row=sc.nextInt();
+		int cols=sc.nextInt();
+		int[][] matrix= new int [row][cols];
+		System.out.println(matrix.length);
+		for(int i=0;i<row;i++) {
+			for(int j=0;j<cols;j++) {
+				matrix[i][j]=sc.nextInt();
+			}
+		}
+		for(int i=0;i<row;i++) {
+			for(int j=0;j<cols;j++) {
+				System.out.print(matrix[i][j]+"\t");
 			}
 			System.out.println();
 			
